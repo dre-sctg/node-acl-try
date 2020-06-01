bd299ee1b567b7f672ae1004d3a3983f26bb2cba
Andrea Cadeddu <acadeddu@servercentral.com>
2020-06-01 16:28:49 -0500

    better example but i can't make work the custom permission check

--- index.js
+++ index.js
@@ -6,23 +6,45 @@ let grantsObject = {
     admin: {
         grants: [
             {
-                resource: '/video', action: '*', attributes: ['*']
+                resource: '/video', 
+                action: '*', 
+                attributes: ['*'] //field specifics, eg only title
+//                 const permission = ac.can('user').execute('read').on('account');
+// permission.granted;       // true
+// permission.attributes;    // ['*', '!record.id']
+// permission.filter(data);  // filtered data (without record.id)
+            },
+            {
+                resource:'/video/*',
+                action:'*',
+                attributes:['*']
             }
         ]
     },
     user: {
         grants: [
             {
-                resource: 'video', action: 'create', attributes: ['*']
+                resource: '/video', action: 'GET', attributes: ['*']
             },
             {
-                resource: 'video', action: 'read', attributes: ['*']
+                resource: '/video/*', 
+                // action: ['*', '!DELETE'],
+                action:'GET',
+                attributes: ['*']
             },
             {
-                resource: 'video', action: 'update', attributes: ['*']
+                resource: '/video/*/comments', 
+                action: ['GET','POST'], 
+                attributes: ['*']
             },
             {
-                resource: 'video', action: 'delete', attributes: ['*']
+                resource: 'video/*',
+                action: 'DELETE',
+                attributes: ['*'],
+                condition: {
+                            Fn: 'custom:isResourceOwner',
+                            args: { resource: 'article' }
+                        }
             },
         ]
     },
@@ -90,10 +112,11 @@ const customConditions={
             return type && getValueByPath(context, '$.category.type') === type;
         },
         isResourceOwner: (context, { resource } = {}) => {
+            console.log('custom condition: context: ', context,'resource', resource)
             if (!resource) {
                 return false;
             }
-            return getValueByPath(context, `$.${resource}.owner`) === getValueByPath(context, '$.user.id');
+            return getValueByPath(context, `$.${resource}.owner`) === getValueByPath(context, '$.user.name');
         },
     }
 }
@@ -128,13 +151,29 @@ const users={
     }
 }
 
+const resourceTypes = {
+    article:{
+        type: 'article',
+    },
+    default:{}
+}
+
+const videos = {
+    123: {
+        owner:'frank',
+        type:'news',
+        title: 'test of my powers'
+    },
+    default:{}
+}
 
 
 
 // set userinfo
 app.use((req, res, next) => {
   res.locals.user = users[req.query.username||'default']; 
-  console.log('res.locals.user:', res.locals.user) // group/policy lookup
+  res.locals.resource = resourceTypes[req.query.rtype||'default']
+  console.log('res.locals:', res.locals) // group/policy lookup
   console.log(req.path, req.method);
   next();
 });
@@ -145,6 +184,7 @@ app.use(async (req, res, next) => {
   const permission = await ac
     .can(res.locals.user.role)
     .execute(req.method)
+    .with(res.locals) // sets context, eg category
     .on(req.path);
   console.log(permission,res.locals.user, req.method,req.path)
   if (permission.granted){
